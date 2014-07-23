909d53267b7bb3324d5dfd3dbead226e0226167b
Coney Wu <coney.wu@gmail.com>
2014-07-24 00:51:31 +0800

    complete install.sh to install 8188eu driver for rapsi

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+DRIVER=driver/8188eu-20140705.tar.gz
+TARGET=/lib/modules/`uname -r`/kernel/drivers/net/wireless/8188eu.ko
+
+tar xvf $DRIVER -C tmp
+sudo install -p -m 644 tmp/8188eu.ko $TARGET
+sudo insmod $TARGET
+sudo depmod -a
+
+
